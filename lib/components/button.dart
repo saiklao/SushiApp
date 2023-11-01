b8a36b5a54728830c165c05b5359258644a34f56
saisaikl <u5748124@au.edu>
2023-11-01 20:48:48 +0700

    first commit

---
+++ lib/components/button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../theme/colors.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+  const MyButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: secondaryColor, borderRadius: BorderRadius.circular(40)),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // text
+            Text(
+              text,
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 10),
+            // icon
+            Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
